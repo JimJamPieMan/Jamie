3e64b799df0e468633e4dc690dbf6853751d011d
JimJamPieMan <rudlandjames@gmail.com>
2018-04-01 16:51:21 +0930

    aaaa

--- bot.js
+++ bot.js
@@ -142,8 +142,23 @@ Bot.on("message", async message => {
   } else {
   
     if(command === "birthday"){
-      var birthPerson = args[0];
-      var birthDate = args[1];
+      var sampleObject = {
+        a: 1,
+        b: 2,
+        c: {
+          x: 11,
+          y: 22
+        }
+      };
+
+      fs.writeFile("./birthday.json", JSON.stringify(sampleObject), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        };
+        console.log("File has been created");
+      });
+
     }
     
     if (command ==="test"){
