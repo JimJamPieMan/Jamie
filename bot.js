93d216a77d098363a9c0c8a281356b5dd052792c
Glitch (jamie) <none>
2018-06-06 03:37:34 +0000

    💘🚝 Updated with Glitch

--- bot.js
+++ bot.js
@@ -335,7 +335,7 @@ Bot.on("message", async message => {
   
   if (command==="download"){
       var file = fs.createWriteStream("soundsfile.mp3");
-var request = http.get("http://cdn.glitch.com/ed065e92-daf8-4718-90ec-7b7d3c3337ce%2F7.mp3?1518669335850", function(response) {
+var request = http.get("https://vocaroo.com/i/s14V81NWDYRz", function(response) {
   response.pipe(file);
 });
    
